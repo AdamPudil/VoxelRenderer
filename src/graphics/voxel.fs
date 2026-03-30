2afe17d77d420e53b204f0b53d73a88620ea6337
AdamPudil <104brutus104@gmail.com>
2026-03-30 15:57:43 +0200

    updatet shaders.

--- src/graphics/voxel.fs
+++ src/graphics/voxel.fs
@@ -24,7 +24,7 @@ const int CHUNK_SIZE = 16;
 const int CHUNK_VOXELS = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;   // 4096
 const int BITMAP_WORDS = CHUNK_VOXELS / 32;                      // 128
 
-const int renderDistance = 256;
+const int renderDistance = 1024;
 const vec4 skyColor = vec4(0.4, 0.1, 0.1, 1.0);
 
 int floorDiv(int a, int b) {
